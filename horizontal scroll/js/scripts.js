af0f5c7b0baae73963fd5adf0fbafc4e2aa6ce85
danula-ded <117920938+danula-ded@users.noreply.github.com>
2023-11-04 17:22:00 +0300

    main

---
+++ horizontal scroll/js/scripts.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+    // direction: 'vertical',
+    speed: 2400,
+    mousewheel:{
+        enabled: true,
+        sensitivity: 2.4,
+    },
+    spaceBetween: 18,
+    parallax: true,
+    freeMode: true,
+})
